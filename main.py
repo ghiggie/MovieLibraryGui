f515f24cef6576e76fa65f64de8f3ba14e3659f4
Garrett Higginbotham <ghiggie@uab.edu>
2021-05-01 11:28:53 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import List, Dict, Union
+
+creator: str = "Garrett Higginbotham"
+project: str = "GUI for Movie Library"
+version: str = "0.1"
+
+
+
+
+def setup_gui() -> None:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    setup_gui()
+    main()
